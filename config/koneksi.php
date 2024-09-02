4573d6d961770d8f3f984844046b33a0fe3ad70e
bilkis2007 <belkisameliaicit@gmail.com>
2024-09-02 11:39:17 +0800

    Add files via upload

---
+++ config/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$hostname = 'localhost';
+$userdb = 'root';
+$passdb = '';
+$namedb = 'ukk_galerifoto_bilkiss';
+
+$koneksi = mysqli_connect($hostname, $userdb, $passdb, $namedb);
+
+
+?>
---
+++ database/ukk_galerifoto_bilkiss.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 02, 2024 at 03:33 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukk_galerifoto_bilkiss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+DROP TABLE IF EXISTS `album`;
+CREATE TABLE IF NOT EXISTS `album` (
+  `albumid` int NOT NULL AUTO_INCREMENT,
+  `namaalbum` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `deskripsi` text COLLATE utf8mb4_general_ci NOT NULL,
+  `tanggalbuat` date NOT NULL,
+  `userid` int NOT NULL,
+  PRIMARY KEY (`albumid`),
+  KEY `UserID` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`albumid`, `namaalbum`, `deskripsi`, `tanggalbuat`, `userid`) VALUES
+(37, 'Komputer', 'komputer', '2024-08-29', 8),
+(38, 'Mobil', 'Mobil', '2024-09-02', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+DROP TABLE IF EXISTS `foto`;
+CREATE TABLE IF NOT EXISTS `foto` (
+  `fotoid` int NOT NULL AUTO_INCREMENT,
+  `judulfoto` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `deskripsifoto` text COLLATE utf8mb4_general_ci NOT NULL,
+  `tanggalunggah` date NOT NULL,
+  `lokasifile` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `albumid` int NOT NULL,
+  `userid` int NOT NULL,
+  PRIMARY KEY (`fotoid`),
+  KEY `UserID` (`userid`),
+  KEY `AlbumID` (`albumid`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `foto`
+--
+
+INSERT INTO `foto` (`fotoid`, `judulfoto`, `deskripsifoto`, `tanggalunggah`, `lokasifile`, `albumid`, `userid`) VALUES
+(63, 'Mobil', 'Mobil Canggih', '2024-09-02', '1857022362-mobil.jpg', 38, 8),
+(64, 'Komputer', 'Laptop Canggih', '2024-09-02', '1363003432-komputer jpg.png.png', 37, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentarfoto`
+--
+
+DROP TABLE IF EXISTS `komentarfoto`;
+CREATE TABLE IF NOT EXISTS `komentarfoto` (
+  `komentarid` int NOT NULL AUTO_INCREMENT,
+  `fotoid` int NOT NULL,
+  `userid` int NOT NULL,
+  `isikomentar` text COLLATE utf8mb4_general_ci NOT NULL,
+  `tanggalkomentar` date NOT NULL,
+  PRIMARY KEY (`komentarid`),
+  KEY `FotoID` (`fotoid`),
+  KEY `UserID` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `komentarfoto`
+--
+
+INSERT INTO `komentarfoto` (`komentarid`, `fotoid`, `userid`, `isikomentar`, `tanggalkomentar`) VALUES
+(6, 64, 9, 'wihh', '2024-09-02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likefoto`
+--
+
+DROP TABLE IF EXISTS `likefoto`;
+CREATE TABLE IF NOT EXISTS `likefoto` (
+  `likeid` int NOT NULL AUTO_INCREMENT,
+  `fotoid` int NOT NULL,
+  `userid` int NOT NULL,
+  `tanggallike` date NOT NULL,
+  PRIMARY KEY (`likeid`),
+  KEY `FotoID` (`fotoid`),
+  KEY `UserID` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `likefoto`
+--
+
+INSERT INTO `likefoto` (`likeid`, `fotoid`, `userid`, `tanggallike`) VALUES
+(65, 64, 8, '2024-09-02'),
+(66, 63, 9, '2024-09-02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `userid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `namalengkap` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `password`, `email`, `namalengkap`, `alamat`) VALUES
+(8, 'aa', '4124bc0a9335c27f086f24ba207a4912', 'aa@gmail.com', 'aa', 'aa'),
+(9, 'bil', 'b3188f47d2eac7efc3f1258dc673a9fe', 'bil@gmail.com', 'bil', 'Kamboja');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `foto`
+--
+ALTER TABLE `foto`
+  ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foto_ibfk_2` FOREIGN KEY (`albumid`) REFERENCES `album` (`albumid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
